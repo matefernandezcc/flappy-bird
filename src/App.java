d434cd57c655c772f05c6a31c8f5eeb808f5b1b7
Mateo <matefernandezc@gmail.com>
2024-09-24 22:58:38 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -6,19 +6,19 @@ public class App {
         int boardHeigth = 640;
 
 
-        /* Ventana de la aplicacion */
+        //////////////////////// Ventana de la aplicacion ////////////////////////
         JFrame frame = new JFrame("Flappy Bird");
-        // frame.setVisible(true);
         frame.setSize(boardWidth, boardHeigth);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        //////////////////////// Instanciamiento del Objeto ////////////////////////
         FlappyBird flappyBird = new FlappyBird();
         frame.add(flappyBird);
         frame.pack();
+        flappyBird.requestFocus();
         frame.setVisible(true);
-
     }
 
 };
